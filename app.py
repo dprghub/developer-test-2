5726a629e8d092cd116c5fe1c0eb7f193bbe12ea
Dean Raemaekers <dprghub@gmail.com>
2020-05-20 19:38:58 +0200

    currency converter

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import snoop
 from flask import Flask, render_template, request
 from peewee import *
-
+import datetime
 from models import database_proxy, History
+import requests
+import requests_cache
+requests_cache.install_cache(cache_name='currency_cache', backend='sqlite', expire_after=3600)
 
+import pickle 
 app = Flask(__name__)
 
 database = SqliteDatabase('test.db')
@@ -14,14 +19,20 @@ database.create_tables([History])
 def index():
     return render_template('index.html')
 
+@snoop
 @app.route('/convert', methods=['POST'])
 def convert():
     # TODO: do conversion of currency
+    date = datetime.datetime.now()
     amount = request.form['amount']
-    src_currency = request.form['src_currency']
-    tgt_currency = request.form['tgt_currency']
-
-    result = 0.0
+    src_currency = request.form['src_currency'].upper()
+    tgt_currency = request.form['tgt_currency'].upper()
+    
+    response = requests.get(f"https://free.currconv.com/api/v7/convert?q={src_currency}_{tgt_currency}&compact=ultra&apiKey=09cb0e075a44116bd021")
+    result = response.json()[f"{src_currency}_{tgt_currency}"]
+    result = float(result)*int(amount)
+    history = History(result=result, tgt_amount=int(amount), src_amount=int(amount), src_currency=src_currency, tgt_currency=tgt_currency, date=date)
+    history.save()
 
     return render_template('convert.html', result=result)
 
--- models.py
+++ models.py
@@ -1,15 +1,15 @@
 from peewee import *
-
-
+import datetime
 database_proxy = DatabaseProxy()
 
 class BaseModel(Model):
     class Meta:
         database = database_proxy
 
-
 class History(BaseModel):
-    src_amount = FloatField()
+    date = DateTimeField(default=datetime.datetime.now)
+    src_amount = IntegerField()
+    tgt_amount = IntegerField()
     src_currency = CharField()
-    tgt_amount = FloatField()
     tgt_currency = CharField()
+    result = FloatField()
